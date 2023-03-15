31d58da03d7fc834939fdb14595df9df95b0c1f6
Vitaliy <23239931+vitaliy-mogilevskiy@users.noreply.github.com>
2023-03-15 18:04:29 +0200

    new lesson js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// var time = 3;
+// var speedOfFirstCyclist = 12;
+// var speedOfSecondCyclist = 14;
+// //var distanceOfFirstCyclist = speedOfFirstCyclist * time;
+// //var distanceOfSecondCyclist = speedOfSecondCyclist * time;
+// //var totalDistance = distanceOfFirstCyclist + distanceOfSecondCyclist;
+// var calculateDistance = function() {
+//     var distanceOfFirstCyclist = speedOfFirstCyclist * time;
+//     var distanceOfSecondCyclist = speedOfSecondCyclist * time;
+//     var totalDistance = distanceOfFirstCyclist + distanceOfSecondCyclist;
+//     return totalDistance;
+//     console.log(totalDistance)
+// };
+// calculateDistance();
+
+
+// var buySomeBread = function(eggs){
+//     if (eggs){
+//         return 10;
+//     } else {
+//         return 1;
+//     }
+// };
+// console.log(buySomeBread(true));
+
+var result = true ? 1 : 0;
+console.log(result);
